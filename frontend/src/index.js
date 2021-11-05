20c37d0cde20cee75939ceb2f0588f7c632f58ff
davidvous <ledavidvu@gmail.com>
2021-11-05 13:40:50 -0700

    Finished modal

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as sessionActions from './store/session';
-
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { ModalProvider } from './context/Modal';
 
+
+import * as sessionActions from './store/session';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 
@@ -25,9 +26,11 @@ if (process.env.NODE_ENV !== 'production') {
 function Root() {
     return (
         <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
+            <ModalProvider>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </ModalProvider>
         </Provider>
     );
 }
