7f611dd39684e0eb8eab85cd14a1207f24d6afda
davidvous <ledavidvu@gmail.com>
2021-11-05 10:33:53 -0700

    signup react component done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
+import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 
 function App() {
@@ -10,10 +11,14 @@ function App() {
     useEffect(() => {
         dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
     }, [dispatch]);
+
     return isLoaded && (
         <Switch>
             <Route path="/login">
                 <LoginFormPage />
+            </Route>
+            <Route path="/signup">
+                <SignupFormPage />
             </Route>
         </Switch>
     );
