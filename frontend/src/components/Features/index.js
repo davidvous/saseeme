e0a5a815589fe3cda346ddf3f8b0cb9fadc17679
davidvous <ledavidvu@gmail.com>
2021-11-08 14:53:59 -0800

    added features and footer components

---
+++ frontend/src/components/Features/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Features.css';
+
+function Features() {
+
+    return (
+        <div className="FeaturesContainer">
+            <div className="FeaturesContent">
+                <div className="FeaturesLeft">Left Text
+                </div>
+                <div className="FeaturesRight">Right Text
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Features;
