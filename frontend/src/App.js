044101f572d739c50e8043d3cce5b152802a7450
davidvous <ledavidvu@gmail.com>
2021-11-08 14:31:16 -0800

    footer styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 import Maincontent from "./components/Maincontent";
+import Footer from "./components/Footer"
 
 function App() {
 
@@ -15,17 +16,20 @@ function App() {
     }, [dispatch]);
 
     return (
-        <div className="appContainer">
-            <Navigation isLoaded={isLoaded} />
-            {isLoaded && (
-                <Switch>
-                    <Route path="/signup">
-                        <SignupFormPage />
-                    </Route>
-                </Switch>
-            )}
-            <Maincontent isLoaded={isLoaded} />
-        </div>
+        <>
+            <div className="appContainer">
+                <Navigation isLoaded={isLoaded} />
+                {isLoaded && (
+                    <Switch>
+                        <Route path="/signup">
+                            <SignupFormPage />
+                        </Route>
+                    </Switch>
+                )}
+                <Maincontent isLoaded={isLoaded} />
+            </div>
+            <Footer />
+        </>
     );
 }
 
---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+
+    return (
+        <div className="FooterContainer">
+            <div className="FooterContent">
+                <div className="FooterLeft">Left Text
+                </div>
+                <div className="FooterRight">Right Text
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
