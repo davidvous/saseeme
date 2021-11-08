c205fd9558c0883dca739104a3ad286a7b5b6c2b
davidvous <ledavidvu@gmail.com>
2021-11-08 12:11:41 -0800

    pixel matching top navBar

--- frontend/src/components/Guestcontent/index.js
+++ frontend/src/components/Guestcontent/index.js
@@ -6,19 +6,30 @@ function Guestcontent() {
     return (
         <div className="GuestcontentMain">
             <div className="background">
-                <div className="background_columnLeft" />
-                <div>
-                    <img src="https://cdn.discordapp.com/attachments/907133739128217621/907305017021714472/logo_invert.png" />
-                    <img src="https://cdn.discordapp.com/attachments/907133739128217621/907307253009383465/logo.png" />
+                <div className="background_columnLeft">
+                    <div className="logo">
+                        <img src="https://cdn.discordapp.com/attachments/907133739128217621/907305017021714472/logo_invert.png" />
+                    </div>
+                    <div className="logoTextContainer">
+                        <h1 className="logoText">SASEEME</h1>
+                        <p>Yum</p>
+                    </div>
+                    <div className="logoHR">
+                    </div>
+                    <div className="logoExtraText">
+                        <p>Find your favorite dishes and share it with the world!</p>
+                    </div>
                 </div>
-                <div className="bg_logo_text">
-                    <p>Saseeme</p>
-                    <p>Yum</p>
+                <div className="background_columnRight">
+                    <div className="imageCarousel">
+                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/EzNDh4YuvNrJfmPYEpPZzQ/o.jpg" height="200px" width="200px" />
+                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/EzNDh4YuvNrJfmPYEpPZzQ/o.jpg" height="200px" width="200px" />
+                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/EzNDh4YuvNrJfmPYEpPZzQ/o.jpg" height="200px" width="200px" />
+                        <img src="https://s3-media0.fl.yelpcdn.com/bphoto/EzNDh4YuvNrJfmPYEpPZzQ/o.jpg" height="200px" width="200px" />
+                    </div>
                 </div>
-
-                <div className="background_columnRight" />
             </div>
-            <div className="searchbar" />
+            <div className="searchBar" />
             <div className="features" />
         </div>
     );
