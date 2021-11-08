a7538b8dea8a89d98930dd6f9d5ee879559c8635
davidvous <ledavidvu@gmail.com>
2021-11-08 08:31:16 -0800

    styling leftcolumn and image

--- frontend/src/components/Guestcontent/index.js
+++ frontend/src/components/Guestcontent/index.js
@@ -6,20 +6,15 @@ function Guestcontent() {
     return (
         <div className="GuestcontentMain">
             <div className="background">
-                <p>Testing</p>
-                <p>sdfdsfds
-                    dsfds
-                    fromfds
-                    fromfdsfs
-                    fromfdssdf
-                    dsfdsfds
-                    fromfdsfssdf
-                    sdfdsfdsfsd
-                    ffromfdsfssdff
-                    sdfdsfdsfsdds
-                    ffromfdsfssdfff
-                    s</p>
                 <div className="background_columnLeft" />
+                <div>
+                    <img src="https://cdn.discordapp.com/attachments/907133739128217621/907305017021714472/logo_invert.png" />
+                </div>
+                <div className="bg_logo_text">
+                    <p>Saseeme</p>
+                    <p>Yum</p>
+                </div>
+
                 <div className="background_columnRight" />
             </div>
             <div className="searchbar" />
