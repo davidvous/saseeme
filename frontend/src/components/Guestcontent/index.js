9ee1f20729e293d76f374a312af50efc3657747d
davidvous <ledavidvu@gmail.com>
2021-11-08 07:14:39 -0800

    redid guestcontent css

---
+++ frontend/src/components/Guestcontent/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Guestcontent.css';
+
+function Guestcontent() {
+
+    return (
+        <div className="GuestcontentMain">
+            <div className="background">
+                <div className="background_columnLeft" />
+                <div className="background_columnRight" />
+            </div>
+            <div className="searchbar" />
+            <div className="features" />
+        </div>
+    );
+}
+
+export default Maincontent;
