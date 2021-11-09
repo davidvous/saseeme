bed2c46f48faadc9801a6ae7d53eae1d4c19a1b8
davidvous <ledavidvu@gmail.com>
2021-11-09 10:18:36 -0800

    restaurant migrations/models

---
+++ backend/db/migrations/20211109174431-create-restaurants.js
@@ -0,0 +1,60 @@
+'use strict';
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('Restaurants', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            user_id: {
+                allowNull: false,
+                type: Sequelize.INTEGER
+            },
+            title: {
+                allowNull: false,
+                type: Sequelize.STRING(50)
+            },
+            address: {
+                allowNull: false,
+                type: Sequelize.STRING(50)
+            },
+            city: {
+                allowNull: false,
+                type: Sequelize.STRING(50)
+            },
+            state: {
+                allowNull: false,
+                type: Sequelize.STRING(25)
+            },
+            zipCode: {
+                allowNull: false,
+                type: Sequelize.STRING(10)
+            },
+            country: {
+                allowNull: false,
+                type: Sequelize.STRING(25)
+            },
+            lat: {
+                type: Sequelize.DECIMAL(10, 7)
+            },
+            lng: {
+                type: Sequelize.DECIMAL(10, 7)
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.fn('now')
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.fn('now')
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('Restaurants');
+    }
+};
