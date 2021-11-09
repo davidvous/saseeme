88f09641ad30c72b92862b3c668d5143c8b11e68
davidvous <ledavidvu@gmail.com>
2021-11-09 10:30:46 -0800

    checkins migrations, models, and associations

---
+++ backend/db/models/checkins.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    const Checkins = sequelize.define('Checkins', {
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        food_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        comment: DataTypes.TEXT
+    }, {});
+    Checkins.associate = function(models) {
+        Checkins.belongsTo(models.User, { foreignKey: 'user_id' });
+        Checkins.belongsTo(models.Food, { foreignKey: 'food_id' });
+    };
+    return Checkins;
+};
