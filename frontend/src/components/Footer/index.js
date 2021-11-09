70f0e362b5d1f0f3b1915293f061753d6af8f1ff
davidvous <ledavidvu@gmail.com>
2021-11-08 19:38:46 -0800

    added links and font families

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -5,9 +5,13 @@ function Footer() {
 
     return (
         <div className="FooterContent">
-            <div className="FooterLeft">David Le
+            <div className="info">
+                <p> Connect with me: </p>
+                <p className="name"> David Le</p>
             </div>
-        </div>
+            <img height="50px" width="50px" src="https://cdn.discordapp.com/attachments/907133739128217621/907463508101316659/github.png" />
+            <img height="42px" width="160px" src="https://cdn.discordapp.com/attachments/907133739128217621/907465323828084746/linkedin.png" />
+        </div >
     );
 }
 
