127ba800c345da15edb17393f0580d931f68ea43
davidvous <ledavidvu@gmail.com>
2021-11-10 12:05:04 -0800

    confirmed singleFood

--- frontend/src/components/ProfilePage/index.js
+++ frontend/src/components/ProfilePage/index.js
@@ -1,19 +1,8 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getFoods } from "../../store/foods";
 import './ProfilePage.css'
-import Checkins from '../Checkins'
 import Singlefood from '../Singlefood'
 
 const ProfilePage = () => {
-    const dispatch = useDispatch();
-    const foods = useSelector((state) => Object.values(state.foods));
-
-
-    useEffect(() => {
-        dispatch(getFoods());
-    }, [dispatch]);
-
     return (
         <div className="ProfileContainer">
             <Singlefood />
--- frontend/src/components/Singlefood/index.js
+++ frontend/src/components/Singlefood/index.js
@@ -3,24 +3,25 @@ import { useDispatch, useSelector } from "react-redux";
 import { getFoods } from "../../store/foods";
 
 const Singlefood = () => {
-    // const dispatch = useDispatch();
+    const dispatch = useDispatch();
     const foods = useSelector((state) => Object.values(state.foods));
     const oneFood = foods.find(food => food.id == 2);
     const { name, imageUrl, description } = oneFood;
 
-
-    //use effect is triggerd on each render and re-render
-    // useEffect(() => {
-    //     dispatch(getFoods());
-    // }, [dispatch]);
+    useEffect(() => {
+        dispatch(getFoods());
+    }, [dispatch]);
 
     return (
         <div className="SinglefoodContainer">
             <div>
-                <h1>{name}</h1>
-                <img src={imageUrl}></img>
+                <img src={imageUrl} />
+            </div>
+            <div>
+                <span>{name}</span>
+            </div>
+            <div>
                 <span>{description}</span>
-
             </div>
         </div >
     );
