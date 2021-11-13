57c73e920423052f12a454a468f7db79fdc73331
davidvous <ledavidvu@gmail.com>
2021-11-12 20:51:36 -0800

    further tweaks

--- frontend/src/components/createFoodModal/createForm.js
+++ frontend/src/components/createFoodModal/createForm.js
@@ -12,35 +12,36 @@ import CreateCheckin from "./createCheckin";
 function CreateForm({ setShowModal }) {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
+  const userFoods = useSelector((state) => Object.values(state.foods).find(item => item.user_id == sessionUser.id));
   const [page, setPage] = useState(2);
-
-
-    useEffect(() => {
-      dispatch(getFoods());
-      dispatch(getCheckins());
-      dispatch(getRestaurants());
-    }, [dispatch]);
-
-
+  
+  useEffect(() => {
+    dispatch(getFoods());
+    dispatch(getCheckins());
+    dispatch(getRestaurants());
+  }, [dispatch]);
+  
+  
   const FormTitles = [
     "Add a food dish",
     "Choose which restaurant",
     "Checkin and leave a comment!",
   ];
-
+  
   const PageDisplay = () => {
     if (page === 0) {
       return <CreateFood setShowModal={setShowModal} userId={sessionUser.id} setPage={setPage}/>
     } else if (page === 1) {
       return (
         <CreateFoodRes setShowModal={setShowModal} userId={sessionUser.id} setPage={setPage} />
-      );
-    } else if (page === 2) {
-      return <CreateCheckin setShowModal={setShowModal} userId={sessionUser.id} setPage={setPage} />;
-    }
-  };
-  
+        );
+      } else if (page === 2) {
+        return <CreateCheckin setShowModal={setShowModal} userId={sessionUser.id} setPage={setPage} />;
+      }
+    };
+
   return (
+    
     <div className="modal_container">
       <h2>{FormTitles[page]}</h2>
       {PageDisplay()}
